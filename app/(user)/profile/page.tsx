c3eca90222222c34ec233f2c429c801cc9e37243
Nguyễn Hoàng Phúc <nhphuc183@gmail.com>
2024-05-21 12:31:15 +0700

    Get API info user

--- app/(user)/profile/page.tsx
+++ app/(user)/profile/page.tsx
@@ -1,14 +1,51 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import { GET } from "@/app/utils";
+
+interface User {
+  id: number;
+  userName: string;
+  phone: string;
+  email: string;
+  fullName: string;
+  city: string;
+  country: string;
+  createdAt: Date;
+}
+
 const Page = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await GET("v1/user");
+        // @ts-ignore
+        const data = await res.json();
+        if (data && data.data && data.data.length > 0) {
+          setUser(data.data[0]); // Set the first user from the response
+        }
+        console.log(data.data);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex mt-4 gap-4 p-2 bg-slate-300">
       <div className="p-4 rounded-xl font-bold h-1/2 bg-slate-500">
         <div className="w-60 h-60 relative rounded-md overflow-hidden mb-4">
-          <Image src="/avt_admin.webp" alt="" fill />
+          <Image src="/avt_admin.webp" alt="User Avatar" fill />
         </div>
-        LoylP
+        {user.userName}
       </div>
       <div className="flex-auto bg-slate-500 p-4 rounded-xl">
         <div className="text-2xl">Username</div>
@@ -16,34 +53,28 @@ const Page = () => {
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="username"
-          placeholder="user"
+          placeholder={user.userName}
         />
         <div className="text-2xl mt-2">FullName</div>
         <input
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="fullname"
-          placeholder="fullname"
+          placeholder={user.fullName}
         />
         <div className="text-2xl mt-2">Email</div>
         <input
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="email"
-          placeholder="email"
+          placeholder={user.email}
         />
-        {/* <div className="text-2xl">Password</div>
-        <input
-          className="p-2 rounded-md mx-2 bg-slate-600 text-gray-300"
-          type="password"
-          name="password"
-        /> */}
         <div className="text-2xl mt-2">Phone</div>
         <input
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="phone"
-          placeholder="phone"
+          placeholder={user.phone}
         />
         <div className="text-2xl mt-2">Gender</div>
         <input
@@ -57,14 +88,14 @@ const Page = () => {
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="city"
-          placeholder="city"
+          placeholder={user.city}
         />
         <div className="text-2xl mt-2">Country</div>
         <input
           className="p-2 rounded-md mx-2 text-xl bg-slate-600 text-gray-300"
           type="text"
           name="country"
-          placeholder="country"
+          placeholder={user.country}
         />
       </div>
     </div>
